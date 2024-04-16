bc39561bd3b80ebd610e294c8820f4d1d24ea39b
himanshu027dev <163422498+himanshu027dev@users.noreply.github.com>
2024-04-16 23:11:04 +0530

    Add files via upload

---
+++ ass.net/ClassLibraryAA/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Classlibrary{
+    public class MyMathLib{
+
+        public void sqr(int x)
+        {
+            Console.WriteLine("Square of "+x+" is : " + x*x);
+        }
+        protected void cube(int x)
+        {
+            Console.WriteLine("Cube of "+x+" is : " + x*x*x);
+        }
+        protected internal void sum(int x,int y){
+            Console.WriteLine("sum="+(x+y));
+        }
+
+    }
+
+    public class Child:MyMathLib{
+        public void usepmathad(){
+           sum(2,3);
+           cube(3);
+            
+        }
+    }
+    public class usemath{
+        public static void makeObj(){
+            MyMathLib obj = new MyMathLib();
+            obj.sqr(2);
+            
+            obj.sum(2,3);
+        }
+    }
+        
+            
+}
---
+++ ass.net/DAY6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Classlibrary;
+
+namespace refadd{
+    class prac:MyMathLib{
+        
+        public static void Main(string[] args)
+        {
+            Classlibrary.Child ch= new Classlibrary.Child();
+            //Classlibrary.MyMathLib mml= new Classlibrary.MyMathLib();
+            //Classlibrary.usemath um= new Classlibrary.usemath();   
+           // mml.sum(1,3);
+            //mml.cube(4);
+            prac geo= new prac();
+           // geo.sqr(2);
+            ch.usepmathad();
+            usemath.makeObj();
+            geo.docall();
+            calls();
+        }
+        public void docall(){
+            sum(1,2);
+            cube(2);
+            sqr(9);
+        }
+        public static void calls(){
+          // Classlibrary.usemath op=new Classlibrary.usemath();
+           // usemath.makeObj();
+            Classlibrary.Child k=new Classlibrary.Child();
+            k.usepmathad();
+            Classlibrary.MyMathLib klao= new Classlibrary.MyMathLib();
+           // klao.sum(1,2);
+            //klao.cube(4);
+            klao.sqr(2);
+           // sum(2,2);
+        }
+    }
+}
