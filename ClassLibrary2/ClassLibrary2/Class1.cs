f376657af39d14e18bf6fd350f898b1b8a785133
himanshu027dev <163422498+himanshu027dev@users.noreply.github.com>
2024-04-16 23:02:48 +0530

    Add files via upload

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {   
+
+        public int sqr(int x)
+        {
+            return x * x;
+        }
+        internal void dojob()
+        {
+            Console.WriteLine("jobs jobs");
+        }
+
+    }
+}
